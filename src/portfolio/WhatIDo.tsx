dd2fb293078ee4d696b0a431e45ad9098f0da273
Vojta Tomas <tomas@vojtatom.cz>
2022-11-28 15:17:30 +0100

    Adding sketch code

--- src/portfolio/WhatIDo.tsx
+++ src/portfolio/WhatIDo.tsx
@@ -5,7 +5,7 @@ export function WhatIDo() {
     return (
         <div className="whatIDo">
             <div>
-                <span>Interactive visualizaiton</span> on the web, writing <span>Python/C++</span> or <span>TypeScript</span> packages, and <span>CI/CD pipelines</span>, namely for building <span>Python Wheels</span>
+                <span>Interactive visualization</span> on the web, writing <span>Python/C++</span> or <span>TypeScript</span> packages, and <span>CI/CD pipelines</span>, namely for building <span>Python Wheels</span>
             </div>
         </div>
     );
